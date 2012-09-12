01b5bc9247dd105e59d6785697175fd4842ce6c7
Andrew Chilton <andychilton@gmail.com>
2012-09-12 14:06:38 +1200

    Add a homepage

--- app.js
+++ app.js
@@ -28,22 +28,34 @@ var map = {
     'www.awssum.io' : 'awssum.io',
 
     // some test domains
-    'localhost' : 'example.com',
+    'local.host' : 'example.com'
 };
+
+var index = "<!doctype html><head><title>Redirect Service</title></head><body>Coming Soon!</body></html>\n";
 
 // ----------------------------------------------------------------------------
 
 var app = connect();
 
 // some middleware
-app.use(connect.responseTime());
-app.use(uuid());
-app.use(connect.logger());
+// app.use(connect.responseTime());
+// app.use(uuid());
+// app.use(connect.logger());
 
 // the redirection middleware
 app.use(function(req, res, next) {
     // Example.com:8080
     var host = req.headers.host.split(':', 1)[0].toLowerCase();
+
+    // if this is asking for the actual site, check it is for / and show them the index file
+    if ( host === 'redirects.jit.su' || host === 'redirects.nodejitsu.com' || host === 'localhost' ) {
+        if ( req.url === '/' ) {
+            return res.end(index);
+        }
+        // else, not found
+        res.writeHead(404);
+        return res.end('Not Found');
+    }
 
     // see if we know about this host
     if ( map[host] ) {
@@ -52,26 +64,9 @@ app.use(function(req, res, next) {
         return res.end();
     }
 
-    if ( host !== 'redirects.jit.su' ) {
-        // return a Not Found (rather than a server error)
-        res.writeHead(404);
-        res.end('Not Found');
-    }
-
-    next();
-});
-
-// if we reach here, then we don't know about this domain, so check if it's 
-app.use(function(req, res, next) {
-    // see if this request is for
-    var host = req.headers.host.split(':', 1)[0].toLowerCase();
-    if ( host !== '' ) {
-        // return a Not Found (rather than a server error)
-        res.writeHead(404);
-        res.end('Not Found');
-    }
-
-    next();
+    // we don't know about this domain, so 404 it
+    res.writeHead(404);
+    res.end('Not Found');
 });
 
 // start up the app
